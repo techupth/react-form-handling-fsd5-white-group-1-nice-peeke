46aed82305ab203e92100b2bbd6fa6c1ba43f140
iHEDISNICE <hedis_nice@yahoo.com>
2023-12-21 16:44:43 +0700

    feat : add complete exercise

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,62 +1,80 @@
-function ProductForm() {
+import React, { useState } from "react";
+
+const ProductForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: "",
+    description: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const showDataFromField = () => {
+    const dataToShow = `
+      {
+        "name": "${formData.name}",
+        "price": ${formData.price},
+        "image": "${formData.image}",
+        "description": "${formData.description}"
+      }
+    `;
+    alert(dataToShow);
+  };
+
+  const inputFields = ["Name", "Image Url", "Price", "Description"].map(
+    (field) => ({
+      name: field,
+      label: field,
+      type: field === "Description" ? "textarea" : "text",
+      placeholder: `Enter ${field} here`,
+    })
+  );
+
   return (
-    <form className="post-form">
+    <form
+      className="post-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        showDataFromField();
+      }}
+    >
       <h1>Create Product Form</h1>
-      <div className="input-container">
-        <label>
-          Name
-          <input
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Enter name here"
-            onChange={() => { }}
-          />
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Image Url
-          <input
-            id="image"
-            name="image"
-            type="text"
-            placeholder="Enter image url here"
-            onChange={() => { }}
-          />
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Price
-          <input
-            id="price"
-            name="price"
-            type="number"
-            placeholder="Enter price here"
-            onChange={() => { }}
-          />
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Description
-          <textarea
-            id="description"
-            name="description"
-            type="text"
-            placeholder="Enter description here"
-            onChange={() => { }}
-            rows={4}
-            cols={30}
-          />
-        </label>
-      </div>
+      {inputFields.map(({ name, label, type, placeholder }) => (
+        <div className="input-container" key={name}>
+          <label>
+            {label}
+            {type === "textarea" ? (
+              <textarea
+                id={name}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                onChange={handleChange}
+                value={formData[name]}
+                rows={4}
+                cols={30}
+              />
+            ) : (
+              <input
+                id={name}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                onChange={handleChange}
+                value={formData[name]}
+              />
+            )}
+          </label>
+        </div>
+      ))}
       <div className="form-actions">
         <button type="submit">Create</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProductForm;
